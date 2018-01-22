6d73b00cf93f6abf4b3ce0bedfa99fb6eb687a61
Justin Huffman <jhuffman@hchb.com>
2018-01-22 15:26:34 -0500

    Added comments

--- Calculators.cs
+++ Calculators.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Text;
+using System.Text; //JUSTIN HUFFMAN
 
 namespace Calculators
 {
@@ -8,20 +8,20 @@ namespace Calculators
 		static void Main(string[] args)
 		{
 			bool cont = true;
-			while ( cont == true)
+			while ( cont == true) //While loop with cont determines if program exits or loops
 			{
 				Console.WriteLine("");
 				Console.WriteLine("Please choose an option below: ");
-				Console.WriteLine("[1] --- Continuious Addition");
+				Console.WriteLine("[1] --- Continuious Addition"); 		//UI
 				Console.WriteLine("[2] --- Factorial");
 				Console.WriteLine("[3] --- Tip Calculator");
 				Console.WriteLine("[4] --- Imaginary Product");
 				Console.WriteLine("[5] --- Exit");
 				
-				int select = int.Parse( Console.ReadLine() );
+				int select = int.Parse( Console.ReadLine() ); //User selects option from above menu
 				Console.WriteLine("");
 				
-				switch (select)
+				switch (select) //based off user selection
 				{
 					case 1:
 						ContAdd();
@@ -44,11 +44,11 @@ namespace Calculators
 						break;
 				}
 			}
-			Console.WriteLine("Enter any key to exit...");
+			Console.WriteLine("Enter any key to exit..."); //keeps cmd on screen after program ends
 			Console.ReadLine();
 		}
 		
-		static void ContAdd()
+		static void ContAdd() //option 1 above 
 		{
 			int total = 0;
 			int numIn = 1;
@@ -60,7 +60,7 @@ namespace Calculators
 			}
 		}
 		
-		static void Factorial()
+		static void Factorial() //option 2 above
 		{
 			Console.WriteLine("Please enter a number to be factorialized,");
 			int num = int.Parse( Console.ReadLine() );
@@ -72,14 +72,14 @@ namespace Calculators
 			else Console.WriteLine("Does not exist.");
 		}
 		
-		static int fact(int n)
+		static int fact(int n) //called in Factorial() , does the math
 		{
 			if (n == 0) return 0;
 			if (n == 1) return 1;
 			else return n *= fact(n-1);
 		}
 		
-		static void EZTip()
+		static void EZTip() //option 3 above
 		{
 			Console.WriteLine("Enter amount billed: ");
 			float amount = float.Parse( Console.ReadLine() );
@@ -91,13 +91,13 @@ namespace Calculators
 			Console.WriteLine("Total to be paid: ${0}" , amount *(1 + percent));
 		}
 		
-		public struct imagNum
+		public struct imagNum //sets up imaginary number object type for ImagProd() below
 		{
 			public int real;
 			public int imag;
 		}
 		
-		static imagNum product(imagNum n1, imagNum n2)
+		static imagNum product(imagNum n1, imagNum n2) //does math for imaginary num multiplication in ImagProd()
 		{
 			imagNum result;
 			result.real = (n1.real * n2.real) - (n1.imag * n2.imag);
@@ -105,7 +105,7 @@ namespace Calculators
 			return result;
 		}
 		
-		static void ImagProd()
+		static void ImagProd() //option 4 from above
 		{
 			Console.WriteLine("Please enter the real component, then the imaginary (without the i).");
 			imagNum num1, num2;
